a1eb54f8fb6ba88167b171370caf8b2b4287d4fa
birdiesama <46026849+birdiesama@users.noreply.github.com>
2019-01-05 15:22:47 -0500

    Update CreatingCurveUtil.py

--- CreatingCurveUtil.py
+++ CreatingCurveUtil.py
@@ -1,5 +1,6 @@
 import pymel.core as pm ;
 
+"""
 selection_list = pm.ls ( sl = True ) ;
 info_list = [] ;
 
@@ -15,6 +16,12 @@ end_info = info_list[-1] ;
 start_pos = start_info[1] ;
 end_pos = end_info[1] ;
 
-total_dis = ( ( start_pos[0] - end_pos[0] )**2 + ( start_pos[1] - end_pos[1] )**2 + ( start_pos[2]-end_pos[2])**2 ) ** 0.5
-print total_dis ;
+additionalPoints = division - len ( info_list ) ;
 
+total_dis = ( ( start_pos[0] - end_pos[0] )**2 + ( start_pos[1] - end_pos[1] )**2 + ( start_pos[2]-end_pos[2])**2 ) ** 0.5
+"""
+
+division = 3 ;
+selection_list = pm.ls ( sl = True ) ;
+
+startRef = select
